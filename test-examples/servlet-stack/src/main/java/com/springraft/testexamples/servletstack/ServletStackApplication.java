485f915719a0e3de98ffe2f0790813a309fda29d
João <jpsilva9898@gmail.com>
2020-12-07 16:30:53 +0000

    Setup for servlet stack test (#6)
    * Add project from Spring initializr
    
    * Add .gitignore for whole repo

---
+++ test-examples/servlet-stack/src/main/java/com/springraft/testexamples/servletstack/ServletStackApplication.java
@@ -0,0 +1,13 @@
+package com.springraft.testexamples.servletstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletStackApplication.class, args);
+	}
+
+}
