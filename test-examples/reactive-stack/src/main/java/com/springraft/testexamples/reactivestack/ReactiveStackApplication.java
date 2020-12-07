2f7a851b56d9aa1f71c79bd66a7bdf368b6e3e63
João <jpsilva9898@gmail.com>
2020-12-07 16:40:09 +0000

    Add reactive project setup from spring initializr (#7)

---
+++ test-examples/reactive-stack/src/main/java/com/springraft/testexamples/reactivestack/ReactiveStackApplication.java
@@ -0,0 +1,13 @@
+package com.springraft.testexamples.reactivestack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStackApplication.class, args);
+	}
+
+}
