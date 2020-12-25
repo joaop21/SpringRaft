a5160548e44df1830b5d2621e675712ebab125b0
joaop21 <jpsilva9898@gmail.com>
2020-12-25 16:59:13 +0000

    Add Annotations to Counter Model

--- test-examples/servlet-stack/src/main/java/com/springraft/testexamples/servletstack/models/Counter.java
+++ test-examples/servlet-stack/src/main/java/com/springraft/testexamples/servletstack/models/Counter.java
@@ -1,11 +1,18 @@
 package com.springraft.testexamples.servletstack.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Counter {
 
     /*--------------------------------------------------------------------------------*/
@@ -15,40 +22,10 @@ public class Counter {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    /* Value of the counter */
     private long value;
 
     /*--------------------------------------------------------------------------------*/
-
-    /**
-     * TODO
-     * */
-    protected Counter() {
-        this.value = 0;
-    }
-
-    /**
-     * TODO
-     * */
-    public Counter(long id, long value) {
-        this.id = id;
-        this.value = value;
-    }
-
-    /*--------------------------------------------------------------------------------*/
-
-    /**
-     * TODO
-     * */
-    public long getId() {
-        return id;
-    }
-
-    /**
-     * TODO
-     * */
-    public long getValue() {
-        return value;
-    }
 
     /**
      * TODO
