ca2e95fd2563548335a64e08c076c09d74d0d8d6
joaop21 <jpsilva9898@gmail.com>
2021-01-04 12:25:30 +0000

    Remove unnecessary

--- test-examples/reactive-stack/src/main/java/com/springraft/testexamples/reactivestack/controllers/CounterController.java
+++ test-examples/reactive-stack/src/main/java/com/springraft/testexamples/reactivestack/controllers/CounterController.java
@@ -3,30 +3,25 @@ package com.springraft.testexamples.reactivestack.controllers;
 import com.springraft.testexamples.reactivestack.models.Counter;
 import com.springraft.testexamples.reactivestack.services.CommunicationService;
 import com.springraft.testexamples.reactivestack.services.CounterService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("counter")
 public class CounterController {
 
     /*--------------------------------------------------------------------------------*/
 
-    /* Constant Counter, which doesn't change its reference */
-    private final Counter counter = new Counter();
+    /* Inject Communication Service */
+    private final CounterService counterService;
 
     /* Inject Communication Service */
-    @Autowired
-    private CounterService counterService;
-
-    /* Inject Communication Service */
-    @Autowired
-    private CommunicationService communicationService;
+    private final CommunicationService communicationService;
 
     /*--------------------------------------------------------------------------------*/
 
