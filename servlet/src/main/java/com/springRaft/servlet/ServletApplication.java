8cf58339f1ce35ee4a039457a4c0e429a9c34118
joaop21 <jpsilva9898@gmail.com>
2021-02-12 00:02:21 +0000

    Add servlet project structure

---
+++ servlet/src/main/java/com/springRaft/servlet/ServletApplication.java
@@ -0,0 +1,13 @@
+package com.springRaft.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
