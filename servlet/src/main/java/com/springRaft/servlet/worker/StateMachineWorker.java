c26cd09aefdfc37843bef10106c7f316abde058f
joaop21 <jpsilva9898@gmail.com>
2021-02-19 01:22:35 +0000

    Add outbound logic

--- servlet/src/main/java/com/springRaft/servlet/worker/StateMachineWorker.java
+++ servlet/src/main/java/com/springRaft/servlet/worker/StateMachineWorker.java
@@ -1,8 +1,11 @@
 package com.springRaft.servlet.worker;
 
+import com.springRaft.servlet.communication.outbound.OutboundCommunication;
+import com.springRaft.servlet.communication.outbound.REST;
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -12,17 +15,26 @@ public class StateMachineWorker implements Runnable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StateMachineWorker.class);
 
+    private final OutboundCommunication outboundCommunication;
+
+    public StateMachineWorker(ApplicationContext applicationContext) {
+        this.outboundCommunication = applicationContext.getBean(REST.class);
+    }
+
     /* --------------------------------------------------- */
 
     @SneakyThrows
     @Override
     public void run() {
 
-        LOGGER.info("Entrei");
+        LOGGER.info("Started");
 
         Thread.sleep(5000);
 
-        LOGGER.info("Terminei a execução");
+        this.outboundCommunication.appendEntries();
+        this.outboundCommunication.requestVote();
+
+        LOGGER.info("Finished");
 
     }
 
