306a83a19a503c6fc297ced1de0594c885f288ed
joaop21 <jpsilva9898@gmail.com>
2021-04-06 16:54:31 +0100

    Changes after debugging

--- servlet/src/main/java/com/springRaft/servlet/communication/inbound/RaftController.java
+++ servlet/src/main/java/com/springRaft/servlet/communication/inbound/RaftController.java
@@ -3,6 +3,8 @@ package com.springRaft.servlet.communication.inbound;
 import com.springRaft.servlet.communication.message.*;
 import com.springRaft.servlet.consensusModule.ConsensusModule;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("raft")
 @AllArgsConstructor
 public class RaftController implements InboundCommunication {
+
+    /* Logger */
+    private static final Logger log = LoggerFactory.getLogger(RaftController.class);
 
     /* Module that has the consensus functions to invoke */
     private final ConsensusModule consensusModule;
@@ -32,7 +37,11 @@ public class RaftController implements InboundCommunication {
     )
     public ResponseEntity<AppendEntriesReply> appendEntriesEndpoint(@RequestBody AppendEntries appendEntries) {
 
-        return new ResponseEntity<>(this.appendEntries(appendEntries), HttpStatus.OK);
+        AppendEntriesReply reply = this.appendEntries(appendEntries);
+
+        //log.info("\nREQUEST: " + appendEntries.toString() + "\n" + "RESPONSE: " + reply);
+
+        return new ResponseEntity<>(reply, HttpStatus.OK);
 
     }
 
@@ -47,7 +56,11 @@ public class RaftController implements InboundCommunication {
     )
     public ResponseEntity<RequestVoteReply> requestVoteEndpoint(@RequestBody RequestVote requestVote) {
 
-        return new ResponseEntity<>(this.requestVote(requestVote), HttpStatus.OK);
+        RequestVoteReply reply = this.requestVote(requestVote);
+
+        //log.info("\nREQUEST: " + requestVote.toString() + "\n" + "RESPONSE: " + reply);
+
+        return new ResponseEntity<>(reply, HttpStatus.OK);
 
     }
 
