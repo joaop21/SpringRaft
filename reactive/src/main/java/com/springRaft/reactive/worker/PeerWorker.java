833c81ba5aaeff5db5d1189ada4d71e8296ac8b7
joaop21 <jpsilva9898@gmail.com>
2021-04-13 16:27:12 +0100

    Add peer worker working in backgroun on a scheduler

---
+++ reactive/src/main/java/com/springRaft/reactive/worker/PeerWorker.java
@@ -0,0 +1,29 @@
+package com.springRaft.reactive.worker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@Component
+@Scope("prototype")
+
+public class PeerWorker implements Runnable {
+
+    /* Logger */
+    private static final Logger log = LoggerFactory.getLogger(PeerWorker.class);
+
+    /* --------------------------------------------------- */
+
+    @Override
+    public void run() {
+
+        Flux.range(0,10)
+                .map(i -> i+5)
+                .filter(i -> i%2 == 0)
+                .subscribe(i -> log.info(String.valueOf(i)));
+
+    }
+
+}
