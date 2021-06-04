a3e5b53530962276336cecfa34ac7b083ba7e503
joaop21 <jpsilva9898@gmail.com>
2021-06-05 00:58:36 +0100

    Add Raft properties

--- reactive-refactor/src/main/resources/schema-h2.sql
+++ reactive-refactor/src/main/resources/schema-h2.sql
@@ -1,1 +1,21 @@
+CREATE TABLE IF NOT EXISTS state (
+    id BIGINT(255),
+    current_term BIGINT(255),
+    voted_for VARCHAR(255),
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE IF NOT EXISTS log_state (
+    id BIGINT(255),
+    committed_index BIGINT(255),
+    committed_term BIGINT(255),
+    last_applied BIGINT(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS entry (
+    index BIGINT(255),
+    term BIGINT(255),
+    command VARCHAR(max),
+    PRIMARY KEY (index)
+);
