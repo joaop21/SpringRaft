a95a5f6622e1f0fcae5c527031eb370a43cbd937
joaop21 <jpsilva9898@gmail.com>
2021-06-10 22:28:08 +0100

    Add TODO comments in MessageSubscriber

--- reactive-refactor/src/main/java/com/springRaft/reactive/communication/outbound/MessageSubscriber.java
+++ reactive-refactor/src/main/java/com/springRaft/reactive/communication/outbound/MessageSubscriber.java
@@ -6,16 +6,34 @@ import reactor.core.publisher.Mono;
 
 public interface MessageSubscriber {
 
+    /**
+     * TODO
+     * */
     Mono<Void> sendRequestVote(RequestVote requestVote);
 
+    /**
+     * TODO
+     * */
     Mono<Void> sendAuthorityHeartbeat(AppendEntries heartbeat);
 
+    /**
+     * TODO
+     * */
     Mono<Void> sendHeartbeat(AppendEntries heartbeat, String to);
 
+    /**
+     * TODO
+     * */
     Mono<Void> sendAppendEntries(AppendEntries appendEntries, String to);
 
+    /**
+     * TODO
+     * */
     Mono<Void> newClientRequest();
 
+    /**
+     * TODO
+     * */
     Mono<Void> newFollowerState();
 
 }
