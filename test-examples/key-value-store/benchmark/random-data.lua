c3512abebea67c819d9867d2227b099820fc0c41
joaop21 <jpsilva9898@gmail.com>
2021-06-17 13:08:06 +0100

    Add LUA script for benchmarking random data

--- test-examples/key-value-store/benchmark/random-data.lua
+++ test-examples/key-value-store/benchmark/random-data.lua
@@ -3,6 +3,24 @@ math.randomseed(os.time())
 math.random(); math.random(); math.random()
 
 
+-- Generate a random UUID
+function uuid()
+  local seed={'e','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'}
+  local tb={}
+  for i=1,32 do
+      table.insert(tb,seed[math.random(1,16)])
+  end
+  local sid=table.concat(tb)
+  return string.format('%s-%s-%s-%s-%s',
+      string.sub(sid,1,8),
+      string.sub(sid,9,12),
+      string.sub(sid,13,16),
+      string.sub(sid,17,20),
+      string.sub(sid,21,32)
+  )
+end
+
+
 -- Ports for the servers to test
 local ports = {8001,8002,8003}
 -- Possible addresses to connect
@@ -30,6 +48,8 @@ function setup(thread)
    local index = counter % #addrs + 1
    counter = counter + 1
    thread.addr = addrs[index]
+
+   thread:set("uuid", uuid())
 end
 
 
@@ -39,13 +59,12 @@ function init(args)
   req_counter = 0
 
   for i=1, 100, 1 do
-    table.insert(requests, wrk.format("PUT", "/v2/keys/benchmark-key", {}, "value=" .. i))
-    table.insert(requests, wrk.format("GET", "/v2/keys/benchmark-key"))
+    table.insert(requests, wrk.format("PUT", "/v2/keys/" .. uuid, {}, "value=" .. i))
+    table.insert(requests, wrk.format("GET", "/v2/keys/" .. uuid))
   end
-  table.insert(requests, wrk.format("DELETE", "/v2/keys/benchmark-key"))
+  table.insert(requests, wrk.format("DELETE", "/v2/keys/" .. uuid))
 
 end
-
 
 -- Thread request: Invoke requests
 function request()
