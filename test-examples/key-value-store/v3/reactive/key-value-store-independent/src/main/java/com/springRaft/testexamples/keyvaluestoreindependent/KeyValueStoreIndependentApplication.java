8238526bea3337d0ab74ffcd239487e0eec34772
joaop21 <jpsilva9898@gmail.com>
2021-06-28 15:18:09 +0100

    Add initial reactive app (v3)

---
+++ test-examples/key-value-store/v3/reactive/key-value-store-independent/src/main/java/com/springRaft/testexamples/keyvaluestoreindependent/KeyValueStoreIndependentApplication.java
@@ -0,0 +1,13 @@
+package com.springRaft.testexamples.keyvaluestoreindependent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyValueStoreIndependentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyValueStoreIndependentApplication.class, args);
+	}
+
+}
