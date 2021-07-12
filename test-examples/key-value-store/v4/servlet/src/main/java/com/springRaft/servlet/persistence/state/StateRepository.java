07b272c8f9ceebb1178748da78dcdcff00776e1e
joaop21 <jpsilva9898@gmail.com>
2021-07-12 15:36:15 +0100

    Remove a test example app version

--- test-examples/key-value-store/v4/servlet/src/main/java/com/springRaft/servlet/persistence/state/StateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springRaft.servlet.persistence.state;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StateRepository extends JpaRepository<State,Long> {}
